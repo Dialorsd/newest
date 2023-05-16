875589d183aa7fc03deab57583227e2265f10621
Dialorsd <vladislav.stefanihin@kpk-lp.com.ua>
2023-05-16 17:35:23 +0300

    Initial commit

---
+++ resources/views/pizzas/index.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.layout')
+
+@section('content')
+<a href="/" class="back"><- Back</a>
+<div class="flex-center position-ref full-height">
+   
+  <div class="content">
+      
+        <div class="title m-b-md">
+            Pizza List
+        </div>
+
+        @foreach($pizzas as $pizza)
+          <div>
+            {{ $pizza['name'] }} - {{ $pizza['type'] }} - {{ $pizza['base'] }}
+            <a href="pizzas/{{$pizza['id']}}">click to redirect</a>
+          </div>
+        @endforeach
+
+    </div>
+</div>
+@endsection
